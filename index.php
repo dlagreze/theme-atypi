2dd8589548912824aa5cbdd220295a0bb6644725
LAGREZE <lagrezd@gmail.com>
2013-12-15 19:54:31 +0100

    style.css et index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php 
+/**
+ * Index template.
+ *
+ * @package     WordPress
+ * @subpackage  WP From Scratch
+ * @since       1.0
+ * @license  http://www.opensource.org/licenses/gpl-license.php GPL v2.0 (or later)
+ */
